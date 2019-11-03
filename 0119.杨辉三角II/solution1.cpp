473ceb15b30a9900e927f4f4ff9a472dda64a95c
Yves-Liu <750307817@qq.com>
2019-11-03 19:16:24 +0800

    Create solution1.cpp

---
+++ 0119.杨辉三角II/solution1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        if (numRows<1)
+            return {};
+        else if (numRows==1)
+            return {{1}};
+        else if (numRows==2)
+            return {{1},{1,1}};
+        vector<vector<int>> answer(numRows,vector<int>({}));
+        answer.at(0)={1};
+        answer.at(1)={1,1};
+        for (int i=2;i<numRows;++i)
+        {
+            answer.at(i).push_back(int(1));
+            for (int j=1;j<i;++j)
+                answer.at(i).push_back(answer.at(i-1).at(j-1)+answer.at(i-1).at(j));
+            answer.at(i).push_back(int(1));
+        }
+        return answer;
+    }
+};
