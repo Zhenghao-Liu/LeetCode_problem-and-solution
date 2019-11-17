a18d29b304d4640e2855fddf660f7d3783c185d2
Yves-Liu <750307817@qq.com>
2019-11-17 10:20:38 +0800

    Create solution1_sort.cpp

---
+++ 0137.只出现一次的数字II/solution1_sort.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int nums_size=nums.size();
+        for (int i=0;i<nums_size-3;i=i+3)
+            if (nums.at(i)!=nums.at(i+1))
+                return nums.at(i);
+        return nums.at(nums_size-1);
+    }
+};
