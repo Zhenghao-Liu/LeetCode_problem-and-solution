ea3d854f316f72cb67761cf91a31a4f9c1aa224d
Yves-Liu <750307817@qq.com>
2019-11-17 10:21:00 +0800

    Create solution2_hash.cpp

---
+++ 0137.只出现一次的数字II/solution2_hash.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int nums_size=nums.size();
+        unordered_map<int,int> nums_map;
+        for (int i:nums)
+            if (nums_map.find(i)==nums_map.end())
+                nums_map.insert({i,1});
+            else
+            {
+                if (nums_map.at(i)==1)
+                    nums_map.at(i)=2;
+                else
+                    nums_map.erase(i);
+            }
+        return nums_map.begin()->first;
+    }
+};
