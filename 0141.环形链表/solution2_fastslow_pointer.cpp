44735caf7341ed9d2c1b839c39f7bef39a4e1d89
Yves-Liu <750307817@qq.com>
2019-11-18 17:18:05 +0800

    Create solution2_fastslow_pointer.cpp

---
+++ 0141.环形链表/solution2_fastslow_pointer.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if (head==NULL)
+            return false;
+        ListNode* slow,*fast;
+        slow=head;
+        fast=head->next;
+        while (slow!=NULL && fast!=NULL)
+        {
+            if (slow==fast)
+                return true;
+            slow=slow->next;
+            if (fast->next!=NULL)
+                fast=fast->next->next;
+            else
+                return false;
+        } 
+        return false;
+    }
+};
