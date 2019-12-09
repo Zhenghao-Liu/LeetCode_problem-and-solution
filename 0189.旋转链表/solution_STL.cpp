a1862578725069f88e1aa1a19e5dd6ba586d87a2
Yves-Liu <750307817@qq.com>
2019-12-09 18:48:01 +0800

    Delete solution_STL.cpp

--- 0189.旋转链表/solution_STL.cpp
+++
@@ -1,8 +0,0 @@
-class Solution {
-public:
-    void rotate(vector<int>& nums, int k) {
-        int nums_size=nums.size();
-        k=k%nums_size;
-        std::rotate(nums.begin(),nums.end()-k,nums.end());
-    }
-};
