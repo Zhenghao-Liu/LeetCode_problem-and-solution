05b2cfa660a85aa794f2c0b72dff8cc8487d05a6
Yves-Liu <750307817@qq.com>
2020-01-28 20:56:48 +0800

    Create solution_priority_queue.cpp

---
+++ 0264.丑数II/solution_priority_queue.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        priority_queue <double,vector<double>,greater<double> > q;
+        double answer=1;
+        for (int i=1;i<n;++i)
+        {
+            q.push(answer*2);
+            q.push(answer*3);
+            q.push(answer*5);
+            answer=q.top();
+            q.pop();
+            while (!q.empty() && answer==q.top())
+                q.pop();
+        }
+        return answer;
+    }
+};
