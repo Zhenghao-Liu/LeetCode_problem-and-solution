39d2addd6e4f81817934825fff8334c8d5424b18
Yves-Liu <750307817@qq.com>
2020-02-25 13:18:25 +0800

    Update solution.cpp

--- 0007.整数反转/solution.cpp
+++ 0007.整数反转/solution.cpp
@@ -1,28 +1,17 @@
-static const auto SpeedUp = []{
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    return 0;
-}();
 class Solution {
 public:
     int reverse(int x) {
-        if (x==-2147483648||x==0)
-		    return 0;
-        if (x==-2147483412)
-            return -2143847412;
-	    int wei,zhengfu=(x>0?1:-1),num;
-	    num=x=zhengfu*x;
-	    for (wei=0;wei<=10;++wei)
-		    if (num==0)
-			    break;
-		    else
-			    num=num/10;
-	    long long ans=0;
-        for (int i=wei-1;i>=0;--i)
-	    {
-		    ans=(x%10)*pow(10,i)+ans;
-		    x=x/10;
-	    }
-	    return ((ans*zhengfu > INT_MAX || ans*zhengfu < INT_MIN) ? 0 : ans*zhengfu);
+        int sign=x>=0?1:-1;
+        long long long_x=abs((long long)x);
+        string s=to_string(long_x);
+	//手撕reverse(s.begin(),s.end());
+        auto first=s.begin(),last=s.end();
+        while ((first!=last)&&(first!=--last))
+        {
+            std::iter_swap(first,last);
+            ++first;
+        }
+        long_x=stoll(s)*sign;
+        return long_x>INT_MAX || long_x<INT_MIN ? 0 : long_x;
     }
 };
