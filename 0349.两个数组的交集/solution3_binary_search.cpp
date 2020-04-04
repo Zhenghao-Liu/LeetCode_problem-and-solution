c4b1ae098245d47143e0655a1ad042ee1456fa7a
Yves-Liu <750307817@qq.com>
2020-04-04 23:43:56 +0800

    Create solution3_binary_search.cpp

---
+++ 0349.两个数组的交集/solution3_binary_search.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums2.begin(),nums2.end());
+        unordered_set<int> ans;
+        for (int &i:nums1)
+            if (binary_search(nums2,i))
+                ans.insert(i);
+        return vector<int>(ans.begin(),ans.end());
+
+    }
+    bool binary_search(vector<int> & nums,int target)
+    {
+        int left=0,right=nums.size()-1;
+        while (left<=right)
+        {
+            int mid=left+((right-left)>>1);
+            if (nums.at(mid)==target)
+                return true;
+            else if (nums.at(mid)<target)
+                left=mid+1;
+            else
+                right=mid-1;
+        }
+        return false;
+    }
+};
