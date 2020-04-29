4c5b16abc29ce817d5b51e9946e75b96c4dc52c8
Yves-Liu <750307817@qq.com>
2020-04-29 10:12:18 +0800

    Update solution1.cpp

--- 0033.搜索旋转排序数组/solution1.cpp
+++ 0033.搜索旋转排序数组/solution1.cpp
@@ -1,40 +1,34 @@
-/*
-*将数组一分为二判断哪边有序哪边无序，有序则比较两头，无序一边再进行递归运算
-*/
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        if (nums.size()==0)
-            return -1;
-        return search_target(nums,target,0,nums.size()-1);
-    }
-    int search_target(vector<int> &nums,const int& target,int left,int right)
-    {
-        if (right==left)
+        int nums_size=nums.size();
+        int left=0,right=nums_size-1;
+        while (left<=right)
+        {
+            int mid=left+((right-left)>>1);
+            if (nums.at(mid)==target)
+                return mid;
             if (nums.at(left)==target)
                 return left;
-            else
-                return -1;
-        int middle=(left+right)/2;
-        if (nums.at(middle)>nums.at(right))//左边包括middle有序
-            if ( target<=nums.at(middle) && target>=nums.at(left) )
+            if (nums.at(right)==target)
+                return right;
+            //左边有序
+            if (nums.at(mid)>nums.at(left))
             {
-                for (int i=left;i<=middle;++i)
-                    if (nums.at(i)==target)
-                        return i;
-                return -1;
+                if (target>=nums.at(left) && target<=nums.at(mid))
+                    right=mid-1;
+                else
+                    left=mid+1;
             }
+            //右边有序
             else
-                return search_target(nums,target,middle+1,right);   
-        else//右边不包括middle有序
-            if ( target<=nums.at(right) && target>=nums.at(middle+1))
             {
-                for (int i=middle+1;i<=right;++i)
-                    if (nums.at(i)==target)
-                        return i;
-                return -1;
+                if (target>=nums.at(mid) && target<=nums.at(right))
+                    left=mid+1;
+                else
+                    right=mid-1;
             }
-            else
-                return search_target(nums,target,left,middle);
+        }
+        return -1;
     }
 };
