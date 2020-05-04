779dc106d3f4ece88a858c846e09d7a73ee25863
Yves-Liu <750307817@qq.com>
2020-05-04 23:47:48 +0800

    Create solution.sql

---
+++ 0180.连续出现的数字/solution.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+SELECT
+    DISTINCT l1.num AS ConsecutiveNums
+FROM
+    Logs AS l1,
+    Logs AS l2,
+    Logs AS l3
+WHERE
+    l1.Id=l2.Id-1
+    AND l2.Id=l3.Id-1
+    AND l1.Num=l2.Num
+    AND l2.Num=l3.Num;
