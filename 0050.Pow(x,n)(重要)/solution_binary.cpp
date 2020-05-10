c31c624e11e6f4efa05014adf6a0cf9e44423bb6
Yves-Liu <750307817@qq.com>
2020-05-11 00:21:58 +0800

    Update solution_binary.cpp

--- 0050.Pow(x,n)(重要)/solution_binary.cpp
+++ 0050.Pow(x,n)(重要)/solution_binary.cpp
@@ -1,15 +1,15 @@
 //二分法
 class Solution {
 public:
-double myPow(double x, int n) 
-{
-    if (n==0)
-		return 1;
-	else if (n==1)
-		return x;
-	else if (n==-1)
-		return 1.0/x;
-	double multiple=myPow(x*x,n/2),remainder=myPow(x,n%2);
-	return multiple*remainder;
-}
+        double myPow(double x, int n) 
+        {
+        if (n==0)
+                return 1;
+        else if (n==1)
+                return x;
+        else if (n==-1)
+                return 1.0/x;
+        double multiple=myPow(x*x,n/2),remainder=myPow(x,n%2);
+        return multiple*remainder;
+        }
 };
