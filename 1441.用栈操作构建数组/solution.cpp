7d045a9f65e623a4e5ca7e5bcea0baf1db59e370
Yves-Liu <750307817@qq.com>
2020-05-12 00:36:13 +0800

    Update solution.cpp

--- 1441.用栈操作构建数组/solution.cpp
+++ 1441.用栈操作构建数组/solution.cpp
@@ -2,15 +2,13 @@ class Solution {
 public:
     vector<string> buildArray(vector<int>& target, int n) {
         vector<string> ans;
-        int index=0;
+        int index=1;
         vector<int> mask;
-        for (int i=1;i<=n;++i)
-            mask.push_back(i);
         for (int i=0;i<target.size();++i)
         {
-            if (target.at(i)!=mask.at(index))
+            if (target.at(i)!=index)
             {
-                while (target.at(i)!=mask.at(index))
+                while (target.at(i)!=index)
                 {
                     ans.push_back("Push");
                     ans.push_back("Pop");
