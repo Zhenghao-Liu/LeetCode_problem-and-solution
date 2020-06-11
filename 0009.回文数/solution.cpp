c54676323affc5d028e2e36994af391e8b67aba7
Yves-Liu <750307817@qq.com>
2020-06-11 22:49:17 +0800

    Update solution.cpp

--- 0009.回文数/solution.cpp
+++ 0009.回文数/solution.cpp
@@ -1,30 +1,16 @@
-static const auto SpeedUp = []{
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    return 0;
-}();
+//求x的一半是否和另外一半相同
+//但要注意x的长度是奇数的情况，或者x只有一位数的情况
 class Solution {
 public:
     bool isPalindrome(int x) {
-	    if (x<0)
+        if (x<0 || (x%10==0 && x!=0))
             return false;
-        if (x==0)
-            return true;
-        int wei,num=x;
-	    for (wei=0;wei<=10;++wei)
-		    if (num==0)
-			    break;
-		    else
-			    num=num/10;
-	    long long ans=0,y=x;
-        for (int i=wei-1;i>=0;--i)
-	    {
-		    ans=(x%10)*pow(10,i)+ans;
-		    x=x/10;
-	    }
-	    if (ans==y)
-            return true;
-        else
-            return false;
+	    int reverse_x=0;
+        while (x>reverse_x)
+        {
+            reverse_x=reverse_x*10+x%10;
+            x/=10;
+        }
+        return reverse_x==x || x==reverse_x/10;
     }
 };
