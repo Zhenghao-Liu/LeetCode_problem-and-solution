c5b62fc76dce96aef230a09a4ba431a1858639b5
Yves-Liu <750307817@qq.com>
2020-07-14 11:30:01 +0800

    Update show.h

--- 0000.test_project/show.h
+++ 0000.test_project/show.h
@@ -128,5 +128,34 @@ void show(set<T> answer)
 		cout << *p << endl;
 	cout << "----" << endl;
 }
+template <typename K,typename V>
+void show(multimap<K,V> answer)
+{
+	cout << "----" << endl;
+	cout << "size=" << answer.size() << endl
+		 << "~~~~" << endl;
+	for (auto p=answer.begin();p!=answer.end();++p)
+		cout << p->first << ' ' << p->second << endl;
+	cout << "----" << endl;
+}
+
+
+int base=1e9+7;
+int quick_pow(int a,int n)
+{
+    //a^n
+    if (a==0)
+        return 0;
+    if (n==0)
+        return 1;
+    a%=base;
+    if ((n&1)==1)
+        return a*quick_pow(a,n-1)%base;
+    else
+    {
+        int half=quick_pow(a,n/2);
+        return (long long)half*half%base;
+    }
+}
 
 #endif
