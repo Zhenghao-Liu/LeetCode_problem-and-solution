aa43c84b053b60a9f1b5c926112ae3f9164a810c
Yves-Liu <750307817@qq.com>
2020-10-08 21:29:27 +0800

    Create solution.sql

---
+++ 0521.游戏玩法分析II/solution.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+SELECT
+    player_id,
+    device_id
+FROM
+    Activity
+WHERE
+    (player_id,event_date) IN (
+        SELECT
+            player_id,
+            MIN(event_date) AS event_date
+        FROM
+            Activity
+        GROUP BY
+            player_id
+    );
