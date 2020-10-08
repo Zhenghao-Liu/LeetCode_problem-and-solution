7513ab3fb512542bc6af3bb17be9d432467305ac
Yves-Liu <750307817@qq.com>
2020-10-08 23:41:13 +0800

    Create solution2.sql

---
+++ 1084.销售分析III/solution2.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+SELECT
+    A.product_id,
+    B.product_name
+FROM
+    Sales AS A
+    INNER JOIN Product AS B
+ON
+    A.product_id=B.product_id
+GROUP BY
+    A.product_id
+HAVING
+    SUM(IF(A.sale_date BETWEEN '2019-01-01' AND '2019-03-31',1,0))=COUNT(*);
