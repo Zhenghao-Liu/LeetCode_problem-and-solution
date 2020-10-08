fbd615ca4a5ee4c3fc33452a793249d8b08a3ed0
Yves-Liu <750307817@qq.com>
2020-10-08 22:37:30 +0800

    Create solution.sql

---
+++ 0585.2016年的投资/solution.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT
+    SUM(TIV_2016) AS TIV_2016
+FROM
+    insurance
+WHERE
+    (LAT,LON) NOT IN (SELECT LAT,LON FROM insurance GROUP BY LAT,LON HAVING COUNT(*)>1)
+    AND TIV_2015 IN (SELECT TIV_2015 FROM insurance GROUP BY TIV_2015 HAVING COUNT(*)>1);
