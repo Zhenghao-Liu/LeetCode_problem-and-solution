2d6127e616aa741e625e2f796e7ece457841a8b8
Yves-Liu <750307817@qq.com>
2020-10-11 20:48:30 +0800

    Create solution.sql

---
+++ 1211.查询结果的质量和占比/solution.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT
+    query_name,
+    ROUND(AVG(rating/position),2) AS quality,
+    ROUND(AVG(IF(rating<3,1,0))*100,2) AS poor_query_percentage
+FROM
+    Queries
+GROUP BY
+    query_name;
