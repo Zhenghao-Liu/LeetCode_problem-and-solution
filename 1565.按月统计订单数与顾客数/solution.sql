a67c324d4dcf1c21c907edde11a534d89f029e28
Yves-Liu <750307817@qq.com>
2020-10-14 23:19:11 +0800

    Create solution.sql

---
+++ 1565.按月统计订单数与顾客数/solution.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT
+    DATE_FORMAT(order_date,'%Y-%m') AS month,
+    COUNT(invoice) AS order_count,
+    COUNT(DISTINCT customer_id) AS customer_count
+FROM
+    Orders
+WHERE
+    invoice>20
+GROUP BY
+    DATE_FORMAT(order_date,'%Y-%m');
