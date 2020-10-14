8ce55ca7495985f58430da3fe6a56e5e9ba5089c
Yves-Liu <750307817@qq.com>
2020-10-14 23:11:59 +0800

    Create solution1.sql

---
+++ 1543.产品名称格式修复/solution1.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+SELECT
+    LOWER(LTRIM(RTRIM(product_name))) AS product_name,
+    DATE_FORMAT(sale_date,'%Y-%m') AS sale_date,
+    COUNT(sale_id) AS total
+FROM
+    Sales
+GROUP BY
+    LOWER(LTRIM(RTRIM(product_name))),
+    DATE_FORMAT(sale_date,'%Y-%m')
+ORDER BY
+    product_name ASC,
+    sale_date ASC;
