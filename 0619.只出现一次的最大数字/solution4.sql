d46010d8d1a721892afa94d1ca0bacd255599faa
Yves-Liu <750307817@qq.com>
2020-10-16 20:53:56 +0800

    Create solution4.sql

---
+++ 0619.只出现一次的最大数字/solution4.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+SELECT
+    MAX(num) AS num
+FROM
+    (
+        SELECT
+            num
+        FROM
+            my_numbers
+        GROUP BY
+            num
+        HAVING
+            COUNT(*)=1
+    ) AS A;
