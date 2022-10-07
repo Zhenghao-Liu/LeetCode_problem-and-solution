68df2e5b0a549c91bf6f0ab70bd2e7b0f3338984
Yves-Liu <750307817@qq.com>
2022-10-07 20:04:18 +0800

    Update union_find.h

--- 0000.test_project/union_find.h
+++ 0000.test_project/union_find.h
@@ -2,62 +2,72 @@
 #define UNION_FIND
 
 #include <vector>
+
 using namespace std;
 
-class union_find
-{
+class union_find {
 private:
     vector<int> parent;
     vector<int> rank;
+    vector<int> cnt;
 public:
-    union_find()
-    {
+    union_find() {
         parent.clear();
         rank.clear();
+        cnt.clear();
     }
-    
-    union_find(int n)
-    {
-        parent=vector<int>(n);
-        rank=vector<int>(n,0);
+
+    explicit union_find(int n) {
+        parent = vector<int>(n);
+        rank = vector<int>(n, 0);
+        cnt = vector<int>(n, 0);
         init(n);
     }
 
-    void init(int n)
-    {
-        for (int i=0;i<n;++i)
-        {
-            parent.at(i)=i;
-            rank.at(i)=0;
+    void init(int n) {
+        for (int i = 0; i < n; ++i) {
+            parent.at(i) = i;
+            rank.at(i) = 0;
+            cnt.at(i) = 1;
         }
     }
 
-    int find_root(int i)
-    {
-        if (parent.at(i)!=i)
-        {
-            int root=find_root(parent.at(i));
-            parent.at(i)=root;
+    int find_root(int i) {
+        if (parent.at(i) != i) {
+            int root = find_root(parent.at(i));
+            cnt.at(i) = cnt.at(root);
+            parent.at(i) = root;
             return root;
         }
         return i;
     }
 
-    bool union_vertices(int a,int b)
-    {
-        int a_root=find_root(a);
-        int b_root=find_root(b);
-        if (a_root==b_root)
-            return false;
-        if (rank.at(a_root)<rank.at(b_root))
-            parent.at(a_root)=b_root;
-        else
-        {
-            parent.at(b_root)=a_root;
-            if (rank.at(a_root)==rank.at(b_root))
+    int union_vertices(int a, int b) {
+        int a_root = find_root(a);
+        int b_root = find_root(b);
+        if (a_root == b_root)
+            return cnt.at(a_root);
+        if (rank.at(a_root) < rank.at(b_root)) {
+            parent.at(a_root) = b_root;
+            cnt.at(b_root) += cnt.at(a_root);
+            return cnt.at(b_root);
+        } else {
+            parent.at(b_root) = a_root;
+            cnt.at(a_root) += cnt.at(b_root);
+            if (rank.at(a_root) == rank.at(b_root))
                 ++rank.at(a_root);
+            return cnt.at(a_root);
         }
-        return true;
+        return -1;
+    }
+
+    bool is_connected(int a, int b) {
+        return find_root(a) == find_root(b);
+    }
+
+    int get_cnt(int i) {
+        int root = find_root(i);
+        return cnt.at(root);
     }
 };
 
